09a09e646d738855e7a81f8b5d783563f305d774
Janne Raty <jraty@c1r5p16.hive.fi>
2020-10-07 13:27:33 +0300

    deleted unnecessary

--- up_monitor.sh
+++
@@ -1,1 +0,0 @@
-scp -P 50001 /Users/jraty/GitHub/roger_skyline_1/monitor.sh janne@10.11.66.6:/home/janne/monitor_crontab.sh
--- up_update.sh
+++
@@ -1,1 +0,0 @@
-scp -P 50001 /Users/jraty/GitHub/roger_skyline_1/update_script.sh janne@10.11.66.6:/home/janne/update_script.sh
